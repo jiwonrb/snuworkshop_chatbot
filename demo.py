09d176c3f17863fd22a7fde92117f68df486f9d3
jiwonrb <73768088+jiwonrb@users.noreply.github.com>
2024-05-14 14:16:01 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -163,7 +163,7 @@ def generate_summarize(raw_text, callback):
 
 # page title
 st.set_page_config(page_title='🦜🔗 서울대학교 문서 기반 요약 및 QA 챗봇_rb')
-st.title('🦜🔗 서울대학교 문서 기반 요약 및 QA 챗봇')
+st.title('🦜🔗 SNU 문서 기반 요약 및 QA 챗봇_rb')
 
 import os
 api_key = st.sidebar.text_input("Enter your OpenAI API Key", type="password")
